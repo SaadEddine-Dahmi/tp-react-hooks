715d03523753b3a848baa6fa0e70d2ddec9a760f
SaadEddine-Dahmi <dahmisaad7@gmail.com>
2025-04-22 21:56:44 +0100

    Exercice 1.2

--- src/components/ProductSearch.js
+++ src/components/ProductSearch.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 import { ThemeContext } from "../App";
-
+import { LanguageContext } from "../contexts/LanguageContext";
 // TODO: Exercice 1.2 - Utiliser le hook useDebounce
 const useDebounce = (value, delay) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
@@ -18,10 +18,18 @@ const useDebounce = (value, delay) => {
   return debouncedValue
 };
 
-const ProductSearch = () => {
+const ProductSearch = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState("");
   const { isDarkTheme } = useContext(ThemeContext);
   // TODO: Exercice 2.1 - Utiliser le LanguageContext
+  const { language } = useContext(LanguageContext);
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
+    useEffect(() => {
+        if (debouncedSearchTerm) {
+            onSearch(debouncedSearchTerm);
+        }
+    }, [debouncedSearchTerm, onSearch]);
 
   return (
     <div className="mb-4">
@@ -29,7 +37,11 @@ const ProductSearch = () => {
         type="text"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
-        placeholder="Rechercher un produit..."
+        placeholder={
+          language === "fr"
+            ? "Rechercher un produit..."
+            : "Search for a product..."
+        }
         className={`form-control ${isDarkTheme ? "bg-dark text-light" : ""}`}
       />
     </div>
